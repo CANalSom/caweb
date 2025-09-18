beea103a94ab73f23177b4d368031adeb728505b
2024_Study <loopy0106@office.yuhan.ac.kr>
2025-09-18 17:16:55 +0900

    250918 UPDATED

---
+++ VC++/간단한그림판/간단한그림판/resource.h
@@ -0,0 +1,58 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 간단한그림판.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_MY_DIALOG                   102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MY                          107
+#define IDI_SMALL                       108
+#define IDC_MY                          109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define d                               32782
+#define ID_CRIME                        32783
+#define ID_CRIMSON                      32784
+#define ID_WHITE                        32785
+#define ID_BLACK                        32786
+#define ID_1PX                          32787
+#define ID_5PX                          32788
+#define ID_10PX                         32789
+#define ID_RED                          32790
+#define ID_GREEN                        32791
+#define ID_BLUE                         32792
+#define ID_LINE                         32793
+#define ID_RECTANGLE                    32794
+#define ID_ELLIPSE                      32795
+#define ID_FREE                         32796
+#define ID_FREE_LINE                    32797
+#define ID_ELLIPSE_LINE                 32798
+#define ID_RECTANGLE_LINE               32799
+#define ID_RED_LINE                     32800
+#define ID_GREEN_LINE                   32801
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32802
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
