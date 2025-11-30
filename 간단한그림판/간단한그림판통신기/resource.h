64b6611fa1b115e3fa57449320a76d337625a631
2024_Study <loopy0106@office.yuhan.ac.kr>
2025-11-30 19:04:10 +0900

    20251130

---
+++ 간단한그림판/간단한그림판통신기/resource.h
@@ -0,0 +1,41 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 간단한그림판통신기.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_MY_DIALOG                   102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MY                          107
+#define IDI_SMALL                       108
+#define IDC_MY                          109
+#define IDR_MAINFRAME                   128
+#define ID_CRIMSON                      32784
+#define ID_WHITE                        32785
+#define ID_BLACK                        32786
+#define ID_1PX                          32787
+#define ID_5PX                          32788
+#define ID_10PX                         32789
+#define ID_RED                          32790
+#define ID_GREEN                        32791
+#define ID_BLUE                         32792
+#define ID_LINE                         32793
+#define ID_FREE_LINE                    32797
+#define ID_ELLIPSE_LINE                 32798
+#define ID_RECTANGLE_LINE               32799
+#define ID_BLACKFACE                    32803
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
